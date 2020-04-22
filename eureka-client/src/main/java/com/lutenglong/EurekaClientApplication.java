0174f79a04935ecbdede8533a3500b3417ee5e8d
lutenglong <1093012050@qq.com>
2020-04-22 11:13:23 +0800

    eureka非集群

--- eureka-client/src/main/java/com/lutenglong/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/lutenglong/EurekaClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClientApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class,args);
     }
